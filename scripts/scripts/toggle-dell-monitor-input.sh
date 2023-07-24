eaf3d14422dc5855e1747b5dac8570f5b264d67d
Derrik Milligan <derrik.milligan.dev@gmail.com>
2023-07-24 09:52:36 -0600

    Update the toggle script for dell monitors. Added setup for leftwm to install needed packages.

--- scripts/scripts/toggle-dell-monitor-input.sh
+++ scripts/scripts/toggle-dell-monitor-input.sh
@@ -1,7 +1,14 @@
 #!/usr/bin/env bash
 
-if ddcutil getvcp --display $1 60 | grep -q HDMI; then
-  ddcutil setvcp --display $1 60 0x0f
+MONITOR_INFO=$(ddcutil capabilities --brief --display "$1")
+
+if grep -q "S3219D" <<< "$MONITOR_INFO"; then
+  if ddcutil getvcp --display $1 60 | grep -q HDMI; then
+    ddcutil setvcp --display $1 60 0x0f
+  else
+    ddcutil setvcp --display $1 60 0x11
+  fi
 else
-  ddcutil setvcp --display $1 60 0x11
+  echo "Invalid Monitor"
 fi
+

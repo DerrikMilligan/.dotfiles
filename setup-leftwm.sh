ead59092ec59cc92d3aa354e28877d7438c2f8c0
Derrik Milligan <derrik@applicantpro.com>
2023-08-03 09:04:53 -0600

    A few tweaks for setup.

--- setup-leftwm.sh
+++ setup-leftwm.sh
@@ -4,3 +4,7 @@
 # maim and xclip for screenshots
 sudo pacman -S variety maim xclip
 
+rm -rf ~/.config/leftwm/
+
+stow arco-leftwm
+
--- setup.sh
+++ setup.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
+sudo pacman -S neovim stow ttf-monoid-nerd
+
+stow bash
+
+rm -rf ~/.config/nvim/
 
 stow neovim
-stow bash
+
+rm -rf ~/.config/alacritty/
+
 stow alacritty
 

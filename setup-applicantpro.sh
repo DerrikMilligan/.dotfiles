6ed24d77da9da9fcb5be823110965aa74895fd49
Derrik Milligan <derrik@applicantpro.com>
2024-04-17 14:56:12 -0600

    Updated a few dotfiles and the php install script

--- setup-applicantpro.sh
+++ setup-applicantpro.sh
@@ -3,10 +3,10 @@
 # Install all the PHP74 modules
 paru -S \
   php74 \
-  php74-bcmath php74-bz2 php74-calendar php74-cli php74-ctype php74-curl php74-exif php74-fileinfo php74-gd php74-gettext php74-iconv php74-imagick php74-json php74-mbstring php74-mcrypt php74-memcached php74-mysql php74-pcntl php74-pdo php74-pecl php74-posix php74-redis php74-simplexml php74-tokenizer php74-xdebug php74-xmlreader php74-xmlwriter php74-xsl php74-sqlite memcached phpactor
+  php74-bcmath php74-bz2 php74-calendar php74-cli php74-ctype php74-curl php74-dom php74-exif php74-fileinfo php74-gd php74-gettext php74-iconv php74-imgbinary php74-imagick php74-json php74-mbstring php74-mcrypt php74-memcache php74-memcached php74-mysql php74-openssl php74-pcntl php74-pdo php74-pecl php74-posix php74-redis php74-simplexml php74-sockets php74-sqlite php74-tokenizer php74-xdebug php74-xml php74-xmlreader php74-xmlwriter php74-xsl php74-sqlite memcached phpactor
 
 # Link the php74 executable as the main php executable
-sudo mv /usr/bin/php /usr/bin/php82
+sudo mv /usr/bin/php /usr/bin/php83
 sudo ln -s /usr/bin/php74 /usr/bin/php
 
 mkdir ~/projects/
